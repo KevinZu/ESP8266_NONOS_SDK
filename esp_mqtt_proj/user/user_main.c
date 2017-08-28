b359d74529761f22f0f1a948b105b2d6787ad127
esp8266 <zukeqiang@foxmail.com>
2017-08-29 06:43:15 +0800

    update

--- esp_mqtt_proj/user/user_main.c
+++ esp_mqtt_proj/user/user_main.c
@@ -27,6 +27,7 @@
 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 * POSSIBILITY OF SUCH DAMAGE.
 */
+
 #include "ets_sys.h"
 #include "driver/uart.h"
 #include "osapi.h"
@@ -37,6 +38,8 @@
 #include "gpio.h"
 #include "user_interface.h"
 #include "mem.h"
+#include "driver/key.h"
+
 
 MQTT_Client mqttClient;
 
@@ -91,7 +94,7 @@ void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const cha
 	os_free(topicBuf);
 	os_free(dataBuf);
 }
-
+#if 0
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -168,3 +171,372 @@ void user_init(void)
 
 	INFO("\r\nSystem started ...\r\n");
 }
+#else
+
+#include "ets_sys.h"
+#include "gpio.h"
+#include "osapi.h"
+#include "os_type.h"
+#include "user_interface.h"
+#include "espconn.h"
+#include "driver/uart.h"
+
+
+#include "ip_addr.h"
+#include "mem.h"
+
+#include "smartconfig.h"
+#include "airkiss.h"
+
+
+////////////////////////////////////////////////////////////
+#define DEVICE_TYPE 		"gh_599f8b397756" //wechat public number
+#define DEVICE_ID 			"122475" //model ID
+
+#define DEFAULT_LAN_PORT 	12476
+
+LOCAL esp_udp ssdp_udp;
+LOCAL struct espconn pssdpudpconn;
+LOCAL os_timer_t ssdp_time_serv;
+
+uint8_t  lan_buf[200];
+uint16_t lan_buf_len;
+uint8 	 udp_sent_cnt = 0;
+
+const airkiss_config_t akconf =
+{
+	(airkiss_memset_fn)&memset,
+	(airkiss_memcpy_fn)&memcpy,
+	(airkiss_memcmp_fn)&memcmp,
+	0,
+};
+
+
+LOCAL void ICACHE_FLASH_ATTR
+airkiss_wifilan_time_callback(void)
+{
+	uint16 i;
+	airkiss_lan_ret_t ret;
+	
+	if ((udp_sent_cnt++) >30) {
+		udp_sent_cnt = 0;
+		os_timer_disarm(&ssdp_time_serv);//s
+		//return;
+	}
+
+	ssdp_udp.remote_port = DEFAULT_LAN_PORT;
+	ssdp_udp.remote_ip[0] = 255;
+	ssdp_udp.remote_ip[1] = 255;
+	ssdp_udp.remote_ip[2] = 255;
+	ssdp_udp.remote_ip[3] = 255;
+	lan_buf_len = sizeof(lan_buf);
+	ret = airkiss_lan_pack(AIRKISS_LAN_SSDP_NOTIFY_CMD,
+		DEVICE_TYPE, DEVICE_ID, 0, 0, lan_buf, &lan_buf_len, &akconf);
+	if (ret != AIRKISS_LAN_PAKE_READY) {
+		os_printf("Pack lan packet error!");
+		return;
+	}
+	
+	ret = espconn_sendto(&pssdpudpconn, lan_buf, lan_buf_len);
+	if (ret != 0) {
+		os_printf("UDP send error!");
+	}
+	os_printf("Finish send notify!\n");
+}
+
+LOCAL void ICACHE_FLASH_ATTR
+airkiss_wifilan_recv_callbk(void *arg, char *pdata, unsigned short len)
+{
+	uint16 i;
+	remot_info* pcon_info = NULL;
+		
+	airkiss_lan_ret_t ret = airkiss_lan_recv(pdata, len, &akconf);
+	airkiss_lan_ret_t packret;
+	
+	switch (ret){
+	case AIRKISS_LAN_SSDP_REQ:
+		espconn_get_connection_info(&pssdpudpconn, &pcon_info, 0);
+		os_printf("remote ip: %d.%d.%d.%d \r\n",pcon_info->remote_ip[0],pcon_info->remote_ip[1],
+			                                    pcon_info->remote_ip[2],pcon_info->remote_ip[3]);
+		os_printf("remote port: %d \r\n",pcon_info->remote_port);
+      
+        pssdpudpconn.proto.udp->remote_port = pcon_info->remote_port;
+		os_memcpy(pssdpudpconn.proto.udp->remote_ip,pcon_info->remote_ip,4);
+		ssdp_udp.remote_port = DEFAULT_LAN_PORT;
+		
+		lan_buf_len = sizeof(lan_buf);
+		packret = airkiss_lan_pack(AIRKISS_LAN_SSDP_RESP_CMD,
+			DEVICE_TYPE, DEVICE_ID, 0, 0, lan_buf, &lan_buf_len, &akconf);
+		
+		if (packret != AIRKISS_LAN_PAKE_READY) {
+			os_printf("Pack lan packet error!");
+			return;
+		}
+
+		os_printf("\r\n\r\n");
+		for (i=0; i<lan_buf_len; i++)
+			os_printf("%c",lan_buf[i]);
+		os_printf("\r\n\r\n");
+		
+		packret = espconn_sendto(&pssdpudpconn, lan_buf, lan_buf_len);
+		if (packret != 0) {
+			os_printf("LAN UDP Send err!");
+		}
+		
+		break;
+	default:
+		os_printf("Pack is not ssdq req!%d\r\n",ret);
+		break;
+	}
+}
+
+void ICACHE_FLASH_ATTR
+airkiss_start_discover(void)
+{
+	ssdp_udp.local_port = DEFAULT_LAN_PORT;
+	pssdpudpconn.type = ESPCONN_UDP;
+	pssdpudpconn.proto.udp = &(ssdp_udp);
+	espconn_regist_recvcb(&pssdpudpconn, airkiss_wifilan_recv_callbk);
+	espconn_create(&pssdpudpconn);
+
+	os_timer_disarm(&ssdp_time_serv);
+	os_timer_setfn(&ssdp_time_serv, (os_timer_func_t *)airkiss_wifilan_time_callback, NULL);
+	os_timer_arm(&ssdp_time_serv, 1000, 1);//1s
+}
+
+//struct station_config {
+//uint8 ssid[32];
+//uint8 password[64];
+//uint8 bssid_set;
+//uint8 bssid[6];
+//};
+
+
+
+void ICACHE_FLASH_ATTR
+smartconfig_done(sc_status status, void *pdata)
+{
+	//int i;
+	//uint8 ssid[33];
+	//uint8 password[65];
+	//for(i = 0;i < 33;i ++)ssid[i]=0;
+	//for(i = 0;i < 65;i ++)password[i] = 0;
+	
+    switch(status) {
+        case SC_STATUS_WAIT:
+            os_printf("SC_STATUS_WAIT\n");
+            break;
+        case SC_STATUS_FIND_CHANNEL:
+            os_printf("SC_STATUS_FIND_CHANNEL\n");
+            break;
+        case SC_STATUS_GETTING_SSID_PSWD:
+            os_printf("SC_STATUS_GETTING_SSID_PSWD\n");
+			sc_type *type = pdata;
+            if (*type == SC_TYPE_ESPTOUCH) {
+                os_printf("SC_TYPE:SC_TYPE_ESPTOUCH\n");
+            } else {
+                os_printf("SC_TYPE:SC_TYPE_AIRKISS\n");
+            }
+            break;
+        case SC_STATUS_LINK:
+            os_printf("SC_STATUS_LINK\n");
+            struct station_config *sta_conf = pdata;
+
+		//for(i = 0;i < 32;i ++)ssid[i]=sta_conf->ssid[i];
+		//for(i=0;i<64;i++)password[i] = sta_conf->password[i];
+		os_printf("ssid: %s\n",sta_conf->ssid);
+		os_printf("password: %s\n",sta_conf->password);
+		// TODO:  To store ssid and password.
+
+	        wifi_station_set_config(sta_conf);
+	        wifi_station_disconnect();
+	        wifi_station_connect();
+
+            break;
+        case SC_STATUS_LINK_OVER:
+            os_printf("SC_STATUS_LINK_OVER\n");
+            if (pdata != NULL) {
+				//SC_TYPE_ESPTOUCH
+                uint8 phone_ip[4] = {0};
+
+                os_memcpy(phone_ip, (uint8*)pdata, 4);
+                os_printf("Phone ip: %d.%d.%d.%d\n",phone_ip[0],phone_ip[1],phone_ip[2],phone_ip[3]);
+            } else {
+            	//SC_TYPE_AIRKISS - support airkiss v2.0
+				airkiss_start_discover();
+            }
+            smartconfig_stop();
+	system_restart();
+            break;
+    }
+	
+}
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+
+
+LOCAL os_timer_t timer;
+char zt = 1;
+
+void timer_callback(){
+	//if(GPIO_INPUT_GET(FUNC_GPIO2)){
+		
+	//} else {
+	//	os_printf("GPIO2 is 0");
+	//}
+
+	unsigned int status=GPIO_INPUT_GET(FUNC_GPIO2);
+	//os_printf("GPIO2 is : %d\n",status);
+	if(status){
+		os_printf("------GPIO2 is : %d\n",status);
+	} else {
+		os_printf("********%d    %d   \n",0x55,0x55);
+	}
+	
+	if(zt == 1){
+		//GPIO_OUTPUT_SET(GPIO_ID_PIN(2), 0);
+		zt = 0;
+	}else{
+		//GPIO_OUTPUT_SET(GPIO_ID_PIN(2), 1);
+		zt = 1;
+	}
+}
+
+/******************************************************************************
+ * FunctionName : user_rf_cal_sector_set
+ * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
+ *                We add this function to force users to set rf cal sector, since
+ *                we don't know which sector is free in user's application.
+ *                sector map for last several sectors : ABCCC
+ *                A : rf cal
+ *                B : rf init data
+ *                C : sdk parameters
+ * Parameters   : none
+ * Returns      : rf cal sector
+ *******************************************************************************/
+uint32 ICACHE_FLASH_ATTR
+user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        case FLASH_SIZE_64M_MAP_1024_1024:
+            rf_cal_sec = 2048 - 5;
+            break;
+        case FLASH_SIZE_128M_MAP_1024_1024:
+            rf_cal_sec = 4096 - 5;
+            break;
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+//void key_init(void)
+//{
+//	PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO2);
+//	GPIO_DIS_OUTPUT(FUNC_GPIO2);
+//	gpio_output_set(0, 0, 0, BIT2);
+//}
+
+
+void key_down50ms_func(void)
+{
+	os_printf("\n\n+++++++++++++++++      %s\n\n\n",__FUNCTION__);
+	os_printf("key down 50ms!\n");
+}
+
+void key_down5s_func(void)
+{
+	os_printf("+++++++++++++++++      %s\n",__FUNCTION__);
+	os_printf("start smartconfig............!\n");
+	
+	smartconfig_set_type(SC_TYPE_ESPTOUCH); //SC_TYPE_ESPTOUCH,SC_TYPE_AIRKISS,SC_TYPE_ESPTOUCH_AIRKISS
+	wifi_set_opmode(STATION_MODE);
+	smartconfig_start(smartconfig_done);
+	
+}
+
+struct keys_param key;
+struct single_key_param* single_key[1];
+
+
+void my_key_init(void)
+{
+	struct single_key_param *psingle_key;// = //&single_key[0];
+	psingle_key =
+			key_init_single(GPIO_ID_PIN(2),PERIPHS_IO_MUX_GPIO2_U,FUNC_GPIO2,key_down5s_func,key_down50ms_func);
+	key.key_num = 1;
+	//key.single_key = key_init_single(2,2,(key_function)NULL,(key_function)NULL,(key_function)NULL);
+	single_key[0] = psingle_key;
+	key.single_key = single_key;//&psingle_key;
+
+	key_init(&key);
+}
+
+
+void user_init(void)
+{
+	
+	uart_init(BIT_RATE_115200, BIT_RATE_115200);
+	os_delay_us(60000);
+
+	my_key_init();
+
+#if 1
+
+//	smartconfig_set_type(SC_TYPE_ESPTOUCH); //SC_TYPE_ESPTOUCH,SC_TYPE_AIRKISS,SC_TYPE_ESPTOUCH_AIRKISS
+//	wifi_set_opmode(STATION_MODE);
+//	smartconfig_start(smartconfig_done);
+
+
+	CFG_Load();
+
+	MQTT_InitConnection(&mqttClient, sysCfg.mqtt_host, sysCfg.mqtt_port, sysCfg.security);
+	//MQTT_InitConnection(&mqttClient, "192.168.11.122", 1880, 0);
+
+	MQTT_InitClient(&mqttClient, sysCfg.device_id, sysCfg.mqtt_user, sysCfg.mqtt_pass, sysCfg.mqtt_keepalive, 1);
+	//MQTT_InitClient(&mqttClient, "client_id", "user", "pass", 120, 1);
+
+	MQTT_InitLWT(&mqttClient, "/lwt", "offline", 0, 0);
+	MQTT_OnConnected(&mqttClient, mqttConnectedCb);
+	MQTT_OnDisconnected(&mqttClient, mqttDisconnectedCb);
+	MQTT_OnPublished(&mqttClient, mqttPublishedCb);
+	MQTT_OnData(&mqttClient, mqttDataCb);
+
+	WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
+#endif
+
+	INFO("\r\nSystem started ...\r\n");
+
+}
+
+void user_rf_pre_init(){}
+
+
+#endif
